d68219456956b0874d0b0d2b5c7c1be3e1f2a628
Deepak Kumar <93462418+Deepakk-kumar1@users.noreply.github.com>
2025-08-04 03:29:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ const navToggle = document.querySelector('.nav-toggle');
 // step back button
 
 document.getElementById('backLink').addEventListener('click', function(event) {
-    event.preventDefault(); // Link ka default action stop karne ke liye
-    window.history.back();  // Browser ko ek step pehle page pe le jaane ke liye
+    event.preventDefault();
+    window.location.href = '/';
 });
- 
